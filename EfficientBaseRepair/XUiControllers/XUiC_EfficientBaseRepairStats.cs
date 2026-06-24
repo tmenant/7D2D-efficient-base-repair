34fd9a28d80fece0ec8378ec2aae4032168cd7b9
tmenant <91687641+tmenant@users.noreply.github.com>
2026-06-24 18:56:24 +0200

    refactor xuix button states

--- EfficientBaseRepair/XUiControllers/XUiC_EfficientBaseRepairStats.cs
+++ EfficientBaseRepair/XUiControllers/XUiC_EfficientBaseRepairStats.cs
@@ -7,94 +7,47 @@ public class XUiC_EfficientBaseRepairStats : XUiController
 {
 	private static readonly Logging.Logger logger = Logging.CreateLogger<XUiC_EfficientBaseRepairStats>();
 
-	private XUiController btnRefresh;
-
-	private XUiV_Button btnRefresh_Background;
-
-	private XUiController btnOn;
-
-	private XUiV_Button btnOn_Background;
-
-	private XUiController btnUpgrade;
-
-	private XUiV_Button btnUpgrade_Background;
+	private static readonly Color32 onColor = new Color32(250, 255, 163, 255);
+	private static readonly Color32 offColor = Color.white;
 
 	private XUiV_Label lblOnOff;
-
 	private XUiV_Sprite sprOnOff;
-
 	private XUiV_Label lblUpgrade;
-
 	private XUiV_Sprite sprUpgrade;
 
-	private Color32 onColor = new Color32((byte)250, byte.MaxValue, (byte)163, byte.MaxValue);
-
-	private Color32 offColor = (Color32)Color.white;
-
-	private string turnOff => Localization.Get("xuiTurnOff");
-
-	private string turnOn => Localization.Get("xuiTurnOn");
-
-	private string UpgradeOnText => "Upgrade On";
-
-	private string UpgradeOffText => "Upgrade Off";
-
-	private bool lastOn;
-
-	public int WindowWidth;
+	private string RefreshSound => "UseActions/chest_tier4_open";
+	private string PropTurnOff => Localization.Get("xuiTurnOff");
+	private string PropTurnOn => Localization.Get("xuiTurnOn");
+	private string PropUpgradeOn => Localization.Get("ebrUpgradeOn");
+	private string PropUpgradeOff => Localization.Get("ebrUpgradeOff");
 
 	public TEFeatureEBR TileEntity { get; set; }
+	public int WindowWidth { get; set; }
 
 	public override void Init()
 	{
 		base.Init();
 
-		btnRefresh = GetChildById("btnRefresh");
-		btnRefresh_Background = (XUiV_Button)btnRefresh.GetChildById("clickable").ViewComponent;
-		btnRefresh_Background.Controller.OnPress += BtnRefresh_OnPress;
+		GetChildById("btnOn").OnPress += BtnOn_OnPress;
+		GetChildById("btnRefresh").OnPress += BtnRefresh_OnPress;
+		GetChildById("btnUpgrade").OnPress += BtnUpgrade_OnPress;
 
-		btnOn = GetChildById("btnOn");
-		btnOn_Background = (XUiV_Button)btnOn.GetChildById("clickable").ViewComponent;
-		btnOn_Background.Controller.OnPress += BtnOn_OnPress;
+		lblOnOff = GetChildById("lblOnOff").ViewComponent as XUiV_Label;
+		sprOnOff = GetChildById("sprOnOff").ViewComponent as XUiV_Sprite;
 
-		btnUpgrade = GetChildById("btnUpgrade");
-		btnUpgrade_Background = (XUiV_Button)btnUpgrade.GetChildById("clickable").ViewComponent;
-		btnUpgrade_Background.Controller.OnPress += BtnUpgrade_OnPress;
-
-		lblOnOff = (XUiV_Label)GetChildById("lblOnOff").ViewComponent;
-		sprOnOff = (XUiV_Sprite)GetChildById("sprOnOff").ViewComponent;
-
-		lblUpgrade = (XUiV_Label)GetChildById("lblUpgrade").ViewComponent;
-		sprUpgrade = (XUiV_Sprite)GetChildById("sprUpgrade").ViewComponent;
-
-		((XUiV_Label)GetChildById("lblRefresh").ViewComponent).Text = Localization.Get("xuiServerBrowserRefreshList");
+		lblUpgrade = GetChildById("lblUpgrade").ViewComponent as XUiV_Label;
+		sprUpgrade = GetChildById("sprUpgrade").ViewComponent as XUiV_Sprite;
 	}
 
 	public override void Update(float _dt)
 	{
 		base.Update(_dt);
 
-		if (TileEntity == null)
-			return;
+		lblOnOff.SetTextImmediately(TileEntity.IsOn ? PropTurnOff : PropTurnOn);
+		sprOnOff.SetColorImmediately(TileEntity.IsOn ? onColor : offColor);
 
-		if (lastOn != TileEntity.IsOn)
-		{
-			RefreshIsOn(TileEntity.IsOn);
-		}
-
-		RefreshUpgradeOn(TileEntity.UpgradeOn);
-	}
-
-	public override void OnOpen()
-	{
-		base.OnOpen();
-
-		RefreshIsOn(TileEntity.IsOn);
-		RefreshUpgradeOn(TileEntity.UpgradeOn);
-		// RefreshBindings();
-		// RefreshBindingsSelfAndChildren();
-
-		logger.Info($"OnOpen, windowWidth: {WindowWidth}");
+		lblUpgrade.SetTextImmediately(TileEntity.UpgradeOn ? PropUpgradeOff : PropUpgradeOn);
+		sprUpgrade.SetColorImmediately(TileEntity.UpgradeOn ? onColor : offColor);
 	}
 
 	public override void OnClose()
@@ -107,10 +60,6 @@ public class XUiC_EfficientBaseRepairStats : XUiController
 		// 	instance.TEUnlockServer(TileEntity.GetClrIdx(), blockPos, TileEntity.entityId, false);
 		// 	TileEntity.SetModified();
 		// }
-
-		var header = GetChildById("header");
-
-		logger.Info($"OnClose - width: {ViewComponent.Width}, header: {header.ViewComponent.Width}");
 
 		base.OnClose();
 	}
@@ -155,7 +104,7 @@ public class XUiC_EfficientBaseRepairStats : XUiController
 	private void BtnRefresh_OnPress(XUiController _sender, int _mouseButton)
 	{
 		TileEntity.ForceRefresh();
-		Manager.PlayInsidePlayerHead("UseActions/chest_tier4_open");
+		Manager.BroadcastPlay(RefreshSound);
 	}
 
 	private void BtnOn_OnPress(XUiController _sender, int _mouseButton)
@@ -167,47 +116,4 @@ public class XUiC_EfficientBaseRepairStats : XUiController
 	{
 		TileEntity.SwitchUpgrade();
 	}
-
-	private void RefreshUpgradeOn(bool upgradeOn)
-	{
-		if (upgradeOn)
-		{
-			lblUpgrade.Text = UpgradeOffText;
-			if (sprUpgrade != null)
-			{
-				sprUpgrade.Color = onColor;
-			}
-		}
-		else
-		{
-			lblUpgrade.Text = UpgradeOnText;
-			if (sprUpgrade != null)
-			{
-				sprUpgrade.Color = offColor;
-			}
-		}
-	}
-
-	private void RefreshIsOn(bool isOn)
-	{
-		lastOn = isOn;
-
-		if (isOn)
-		{
-			lblOnOff.Text = PropTurnOff;
-			if (sprOnOff != null)
-			{
-				sprOnOff.Color = onColor;
-			}
-		}
-		else
-		{
-			lblOnOff.Text = PropTurnOn;
-			if (sprOnOff != null)
-			{
-				sprOnOff.Color = offColor;
-			}
-		}
-	}
-
 }
